6220630a4b5e2d931897e41b8e5b4adf13d32656
Travis Lekich <ttlekich@gmail.com>
2018-05-14 23:31:34 -0700

    Add: Lecture 3 - Databases and SqlAlchemy

--- lecture_notes/03-databases/create.sql
+++ lecture_notes/03-databases/create.sql
@@ -7,7 +7,6 @@ CREATE TABLE flights (
 
 -- Inserts rows in to flights.
 INSERT INTO flights (origin, destination, duration) VALUES ('New York', 'London', 415);
-INSERT INTO flights (origin, destination, duration) VALUES ('New York', 'London', 415);
 INSERT INTO flights (origin, destination, duration) VALUES ('Shanghai', 'Paris', 760);
 INSERT INTO flights (origin, destination, duration) VALUES ('Istanbul', 'Tokyo', 700);
 INSERT INTO flights (origin, destination, duration) VALUES ('New York', 'Paris', 435);
@@ -27,8 +26,7 @@ SELECT * FROM flights WHERE id = 3;
 SELECT * FROM flights WHERE origin = 'New York';
 
 -- Selects rows with duraction of greater than 500 and all columns from flights.
-SELECT * FROM flights WHERE duraction > 500;
-
+SELECT * FROM flights WHERE duraction > 500; 
 -- Selects rows with destination is paris AND the duration is greater than 500 from flights.
 SELECT * FROM flights WHERE destination = 'Paris' AND duration > 500;
 
@@ -54,3 +52,38 @@ SELECT * FROM flights WHERE origin in ('New York', 'Lima');
 -- Selects all origin entries that has an 'a'. %substring%
 SELECT * FROM flights WHERE origin LIKE '%a%';
 
+DELETE FROM flights WHERE origin='New York';
+
+SELECT * FROM flights ORDER BY duration ASC;
+
+SELECT * FROM flights ORDER BY duration DESC;
+
+SELECT origin, COUNT(*) FROM flights GROUP BY origin;
+
+-- Foreign Keys
+
+CREATE TABLE passengers (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR NOT NULL,
+    flight_id INTEGER REFERENCES flights
+);
+
+INSERT INTO passengers (name, flight_id) VALUES ('Alice', 3);
+INSERT INTO passengers (name, flight_id) VALUES ('Bob', 3);
+INSERT INTO passengers (name, flight_id) VALUES ('Charlie', 4);
+INSERT INTO passengers (name, flight_id) VALUES ('Dave', 4);
+INSERT INTO passengers (name, flight_id) VALUES ('Erin', 6);
+INSERT INTO passengers (name, flight_id) VALUES ('Frank', 8);
+INSERT INTO passengers (name, flight_id) VALUES ('Grace', 8);
+
+-- Joins
+SELECT origin, destination, name FROM flights JOIN passengers ON passengers.flight_id = flights.id;
+
+SELECT origin, destination, name FROM flights JOIN passengers ON passengers.flight_id = flights.id WHERE name = 'Alice';
+
+-- Selects all data from left table
+SELECT origin, destination, name FROM flights LEFT JOIN passengers ON passengers.flight_id = flights.id;
+-- Selects all data from right table
+SELECT origin, destination, name FROM flights RIGHT JOIN passengers ON passengers.flight_id = flights.id;
+
+--Indexing
