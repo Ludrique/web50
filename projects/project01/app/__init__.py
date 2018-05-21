214f58f184849e96f5a1ec44d68d5926b8be8e1a
Travis Lekich <ttlekich@gmail.com>
2018-05-20 21:00:05 -0700

    Added Login and Logout features.

--- projects/project01/app/__init__.py
+++ projects/project01/app/__init__.py
@@ -1,9 +1,10 @@
 import os
 
 from flask import Flask, session, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_session import Session
+from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
@@ -11,5 +12,9 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Login
+login = LoginManager(app)
+login.login_view = "login"
 
 from app import routes, models
